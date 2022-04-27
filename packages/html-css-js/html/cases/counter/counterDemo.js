0e532ee0288062591165c123794c9775301d694a
HongxuanG <1359774872@qq.com>
2022-04-27 18:07:46 +0800

    chore: add pnpm workspace

---
+++ packages/html-css-js/html/cases/counter/counterDemo.js
@@ -0,0 +1,24 @@
+var countTimer = null;
+var count = 0;
+var counterEle = document.querySelector('#counter');
+console.log(counterEle);
+function counter(endTime) {
+    start(endTime);
+}
+// endTime 终点时间戳和当前时间戳的差值
+function start(endTime) {
+    clearTimeout(countTimer);
+    countTimer = setTimeout(function () {
+        if (+new Date > endTime) {
+            clearTimeout(countTimer);
+        }
+        else {
+            var second = (endTime - +new Date()) / 1000; // 秒
+            counterEle.innerText = second + '秒';
+            start(endTime);
+        }
+    }, 30);
+}
+counter(1651940614893);
+// setTimeout只是被设定了在30毫秒后被执行，计算当前时间戳和给出的时间戳的差值进行计算秒数
+// 如果改成1000毫秒，将会1秒计算一次，设置30毫秒的话可以做到毫秒级计算
